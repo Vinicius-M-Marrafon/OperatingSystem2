63f30def86184ec76c431b045a90b066a2abdef7
Vinicius Marrafon (ViMM) <96776865+Vinicius-M-Marrafon@users.noreply.github.com>
2022-11-08 10:32:16 -0300

    Create PathNotFoundException.java

---
+++ Documents/NetBeansProjects/FileSystem.rar (Unzipped Files)/FileSystem/FileSystem/src/Exceptions/PathNotFoundException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.Exceptions;
+
+/**
+ *
+ * @author vinic
+ */
+public class PathNotFoundException extends Exception {
+    public PathNotFoundException(String message) {
+        super(message);
+    }
+}
